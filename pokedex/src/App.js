f0e047de49eab44173491ce66a2e7bc30d7b8d97
Adria Tavares <adria.tavares28@gmail.com>
2022-03-07 15:12:39 -0300

    Criando as pastas e pages do projeto

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Home from "./pages/home/Home";
+import Pokedex from "./pages/pokedex/Pokedex";
+import PokemonDetails from "./pages/pokemonDetails/PokemonDetails";
 
 function App() {
   return (
     <div>
       POKÉDEX
+      {/* <Home/>
+      <Pokedex />
+      <PokemonDetails /> */}
     </div>
   );
 }
---
+++ pokedex/src/pages/pokemonDetails/PokemonDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PokemonDetails() {
+  return (
+    <div>
+      PokemonDetails
+    </div>
+  );
+}
+
+export default PokemonDetails;
